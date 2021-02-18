953e13002b237ed9bba8e7d9dc4281892b3a4fdc
John Duimovich <jduimovich@gmail.com>
2021-02-18 18:26:54 -0500

    local test script

--- localtest.cmd
+++ localtest.cmd
@@ -1,5 +1,4 @@
 
-docker build -t jduimovich/collect-sarif-files .
-
+docker build -t jduimovich/collect-sarif-files . 
 docker build  -f Dockerfile.test -t jduimovich/sarif-test .
 docker run jduimovich/sarif-test snyk.sarif merged.sarif
